b0e5b39a69b47da6e5685548f9bd115ecdcdc4a8
Kregius <konst@Konstantin-PC.>
2025-06-24 09:57:05 +0200

    F: fix push issues

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
     while running:
@@ -11,7 +15,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
