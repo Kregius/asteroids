26a2f02836529288efb0d5343ba3b046d3b1ac2c
Kregius <konst@Konstantin-PC.>
2025-06-24 13:05:50 +0200

    K: first bullet spawn

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# asteroids
+This is my first game project. I hope it works.
+It is relying on pygame 2.6.1 and venv
+
+Controls:
+
+After the game is run in venv a screen will open:
+w : makes ship(triangle) move forward
+s : makes ship go backwards
+a : rotates ship to the left
+d : rotates ship to the right
+
+Game Over:
+
+Game ends when ship colides with an asteroid
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -34,7 +35,14 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        direction = pygame.Vector2(0, 1).rotate(self.rotation)
+        bullet.velocity += direction * PLAYER_SHOOT_SPEED
